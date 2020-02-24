846ec44fd34f47a4dff8f93b2a16c896e68ece50
Makinohara Shoko <MakinoharaShoko@outlook.com>
2020-02-25 00:06:15 +0800

    Change Layout

--- source/js/osx.js
+++ source/js/osx.js
@@ -38,11 +38,11 @@ document.onclick = function (event) {
   var elem = e.srcElement || e.target;
   while (elem) {
 
-    if (elem != document) {
-      if (elem.id == "blogMenu" || elem.id == "blog"
-        || elem.id == "controlMenu" || elem.id == "control"
-        || elem.id == "optionsMenu" || elem.id == "options"
-        || elem.id == "aboutMenu" || elem.id == "about") {
+    if (elem !== document) {
+      if (elem.id === "blogMenu" || elem.id === "blog"
+        || elem.id === "controlMenu" || elem.id === "control"
+        || elem.id === "optionsMenu" || elem.id === "options"
+        || elem.id === "aboutMenu" || elem.id === "about") {
 
         return;
       }
@@ -58,35 +58,35 @@ document.onclick = function (event) {
       return;
     }
   }
-}
+};
 
 function time(){
   //获得显示时间的div
   t_div = document.getElementById('showtime');
-  var now=new Date()
+  var now=new Date();
   //替换div内容
-  var weekday = new Array(7)
-  weekday[0] = "周日"
-  weekday[1] = "周一"
-  weekday[2] = "周二"
-  weekday[3] = "周三"
-  weekday[4] = "周四"
-  weekday[5] = "周五"
-  weekday[6] = "周六"
+  var weekday = new Array(7);
+  weekday[0] = "周日";
+  weekday[1] = "周一";
+  weekday[2] = "周二";
+  weekday[3] = "周三";
+  weekday[4] = "周四";
+  weekday[5] = "周五";
+  weekday[6] = "周六";
   var zero = "0";
   if (now.getMinutes()<10)
     zero = "0";
   else
     zero = "";
   t_div.innerHTML =
-    weekday[now.getDay()]+" "+now.getHours()+":"+zero+now.getMinutes()+"&nbsp&nbsp"
+    weekday[now.getDay()]+" "+now.getHours()+":"+zero+now.getMinutes()+"&nbsp&nbsp";
   //等待一秒钟后调用time方法，由于settimeout在time方法内，所以可以无限调用
   setTimeout(time,1000);
 }
 
 
 function fenxiang() {
-  var str = "我发现了一篇比较有用的文章，赶快看看吧！"
+  var str = "我发现了一篇比较有用的文章，赶快看看吧！";
   str = str + '\r\n\r\n' +'引用标题：'+ document.title;
   str = str + '\r\n' + '引用地址：' + document.location.href;
   clipboardData.setData('text', str);
